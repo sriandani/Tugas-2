a9192aa75fb935e31436e88ccee9bd317f80b703
sriandani <andanyzzz238@gmail.com>
2024-10-19 20:17:28 +0800

    Menjumlahkanduabuahbilangan.py

--- Menjumlahkanduabuahbilangan.py
+++ Menjumlahkanduabuahbilangan.py
@@ -1,12 +1,5 @@
-def main():
-    try:
-        bilangan1 = float(input("Masukkan bilangan pertama: "))
-        bilangan2 = float(input("Masukkan bilangan kedua: "))
-        hasil = bilangan1 + bilangan2
-        
-        print(f"Hasil penjumlahan {bilangan1} dan {bilangan2} adalah {hasil}")
-    except ValueError:
-        print("Input tidak valid. Harap masukkan angka.")
+a=int(input("masukan bilangan pertama : "))
+b=int(input("masukan bilangan kedua : "))
 
-if __name__ == "__main__":
-    main()
+hasil=a + b
+print(f"hasilnya adalah : {hasil}")
